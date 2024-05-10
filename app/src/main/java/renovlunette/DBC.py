d7b1876d3cf61046cd6b43f32bacc3c64504d457
Ereps <erep23@hotmail.com>
2024-05-11 01:04:08 +0200

    handle de quand le module n'est pas installé

--- app/src/main/java/renovlunette/DBC.py
+++ app/src/main/java/renovlunette/DBC.py
@@ -1,14 +1,21 @@
 #Database Connexion
 import os
-import libsql_experimental as libsql
+
+try:
+    import libsql_experimental as libsql
+except ImportError:
+    os.system('pip install libsql_experimental')
+    import libsql_experimental as libsql
 
 url = 'https://renovlunette-project-ereps.turso.io'
 token = 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTUzNzgwOTEsImlkIjoiZGRjZTJkOGQtY2VmOC00MTdmLThhY2YtNmJhMWNjY2I1ZjhiIn0.o_qmZwR-OhPoK4Zp6XRMdG9Ueeiw_axrNEmwJ2fWD5XNeVfr8S5_J5acHsLQI0GuoiYnzYl39kNMDHZSgW57Dg'
+
+
 def connexion(url,token):
     print(F"connecting to {url}")
     conn = libsql.connect(url,
                         auth_token=token)
     conn.execute("CREATE TABLE IF NOT EXISTS  items (id INT PRIMARY KEY,image BLOB,description TEXT,qualitity INT, price REAL unsigned ,contact TEXT, rib TEXT);")
     conn.commit()
-
-connexion(url,token)
+if __name__ == "__main__":
+    connexion(url,token)
