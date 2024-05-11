b37a21f80c2dc66ffc9e8504f307632f42ba64c8
Ereps <erep23@hotmail.com>
2024-05-11 12:15:18 +0200

    remove python

--- app/src/main/java/renovlunette/DB.java
+++ app/src/main/java/renovlunette/DB.java
@@ -20,9 +20,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import com.google.gson.Gson;
-import org.python.core.PyObject;
-import org.python.core.PyString;
-import org.python.util.PythonInterpreter;
 
 public class DB {
     
