63e6f5b7ca47ededdbc154e4868e0b98bb7f1f84
Ereps <erep23@hotmail.com>
2024-05-22 21:55:08 +0200

    from annonce to payement

--- app/src/main/java/renovlunette/DescItemController.java
+++ app/src/main/java/renovlunette/DescItemController.java
@@ -118,7 +118,10 @@ public class DescItemController implements Initializable{
 
     @FXML
     public void buy(ActionEvent actionEvent) throws Exception{
-        //TODO
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/payment.fxml"));
+        Pane root = (Pane) loader.load();
+        Scene scene = (Scene) ((Node) actionEvent.getSource()).getScene();
+        scene.setRoot(root);
     }
 
 
